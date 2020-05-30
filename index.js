c5f8d5115c725defbade9d43fb8668a095670a91
Arramx <arram@tutanota.com>
2020-05-30 19:29:24 +0200

    Added Trigedasleng functionality

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const Discord = require('discord.js')
+const trig = require('trigedasleng-api');
 const client = new Discord.Client()
 
 client.once('ready', () => console.log(`logged in as ${client.user.tag}!`));
@@ -13,8 +14,34 @@ client.on('message', msg => {
             else break;
         }
         if (counter < 1) counter = 1;
-        
+
         msg.channel.send(`pong${'!'.repeat(msg.content.substr(-counter, counter))}`);
+    }
+
+    if (/!trigeng .*/.test(msg.content)) {
+        const search = msg.content.split(' ').slice(1, msg.content.split(' ').length).join('_');
+        trig.search(search, 'trig')
+        .then(json => {
+            const {exactMatch, words} = json;
+            let translation = '';
+
+            if (!exactMatch.length) {
+                if (!words.length) {
+                    msg.channel.send(`\`\`\`
+No entries found
+\`\`\``);
+                } else {
+                    words[0].translations.forEach(obj => translation += `- ${obj.fullText}\n`);
+                    msg.channel.send(`\`\`\`
+${words[0].text} means:\n${translation}\`\`\``);
+                }
+            } else {
+                exactMatch.forEach(word => word.translations.forEach(obj => translation += `- ${obj.fullText}\n`));
+                msg.channel.send(`\`\`\`
+${exactMatch[0].text} means: \n${translation}\`\`\``);
+            }
+        })
+        .catch(console.error);
     }
 });
 
