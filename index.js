4dac92396c6e63fdc2be8c528914549e0637fe8b
Arramx <arram@tutanota.com>
2020-05-31 11:12:49 +0200

    Restructured trig ifs

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const client = new Discord.Client()
 client.once('ready', () => console.log(`logged in as ${client.user.tag}!`));
 
 client.on('message', msg => {
-
     if (/!trigeng .*/.test(msg.content)) {
         const search = msg.content.split(' ').slice(1, msg.content.split(' ').length).join('_');
         trig.search(search, 'trig')
@@ -14,16 +13,16 @@ client.on('message', msg => {
             const {exactMatch, words} = json;
             let translation = '';
 
-            if (exactMatch.length === 0) {
-                if (words.length === 0) {
-                    msg.channel.send(`\`\`\`No entries found\n\`\`\``);
-                } else {
+            if (exactMatch.length) {
+                exactMatch.forEach(word => word.translations.forEach(obj => translation += `- ${obj.fullText}\n`));
+                msg.channel.send(`\`\`\`${exactMatch[0].text} means: \n${translation}\`\`\``);        
+            } else {
+                if (words.length) {
                     words[0].translations.forEach(obj => translation += `- ${obj.fullText}\n`);
                     msg.channel.send(`\`\`\`${words[0].text} means:\n${translation}\`\`\``);
+                } else {
+                    msg.channel.send(`\`\`\`No entries found\n\`\`\``);
                 }
-            } else {
-                exactMatch.forEach(word => word.translations.forEach(obj => translation += `- ${obj.fullText}\n`));
-                msg.channel.send(`\`\`\`${exactMatch[0].text} means: \n${translation}\`\`\``);
             }
         })
         .catch(() => msg.channel.send(`\`\`\`Server error\`\`\``));
