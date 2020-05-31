696a56654eaabbd309d79a2dd8cd767b64274c27
Arramx <arram@tutanota.com>
2020-05-31 11:02:32 +0200

    Added error handling

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 const fs = require('fs');
 const Discord = require('discord.js')
+const trig = require('trigedasleng-api');
 const client = new Discord.Client()
 
 client.once('ready', () => console.log(`logged in as ${client.user.tag}!`));
 
 client.on('message', msg => {
-    if (/!ping\d*/.test(msg.content)) {
-        let counter = 0;
 
-        for (const char of msg.content.split('').reverse().join('')) {
-            if (/\d/.test(char)) counter++;
-            else break;
-        }
-        if (counter < 1) counter = 1;
-        
-        msg.channel.send(`pong${'!'.repeat(msg.content.substr(-counter, counter))}`);
+    if (/!trigeng .*/.test(msg.content)) {
+        const search = msg.content.split(' ').slice(1, msg.content.split(' ').length).join('_');
+        trig.search(search, 'trig')
+        .then(json => {
+            const {exactMatch, words} = json;
+            let translation = '';
+
+            if (exactMatch.length === 0) {
+                if (words.length === 0) {
+                    msg.channel.send(`\`\`\`No entries found\n\`\`\``);
+                } else {
+                    words[0].translations.forEach(obj => translation += `- ${obj.fullText}\n`);
+                    msg.channel.send(`\`\`\`${words[0].text} means:\n${translation}\`\`\``);
+                }
+            } else {
+                exactMatch.forEach(word => word.translations.forEach(obj => translation += `- ${obj.fullText}\n`));
+                msg.channel.send(`\`\`\`${exactMatch[0].text} means: \n${translation}\`\`\``);
+            }
+        })
+        .catch(() => msg.channel.send(`\`\`\`Server error\`\`\``));
     }
 });
 
